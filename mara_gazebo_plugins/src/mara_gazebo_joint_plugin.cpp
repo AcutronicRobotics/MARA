37ccbd7f03161dfd36510d90d6cd4db0311b6981
Alejandro Hernández Cordero <ahcorde@gmail.com>
2019-01-07 10:56:51 +0100

    Remove check of time
    Thanks to @nzlz fot the help

--- mara_gazebo_plugins/src/mara_gazebo_joint_plugin.cpp
+++ mara_gazebo_plugins/src/mara_gazebo_joint_plugin.cpp
@@ -409,10 +409,6 @@ void MARAGazeboPluginRosPrivate::OnUpdate(const gazebo::common::UpdateInfo & _in
 
   double seconds_since_last_update = (_info.simTime - last_update_time_).Double();
 
-  if (seconds_since_last_update < update_period_) {
-    return;
-  }
-
   if(!executing_axis1 && trajectories_position_axis1.size()>0){
     index_trajectory_axis1 = 0;
     executing_axis1 = true;
