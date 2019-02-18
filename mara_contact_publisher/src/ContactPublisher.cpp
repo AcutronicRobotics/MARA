5008e08f1086d1d86d2a5747c3b399d3eb266f8c
Nestor Gonzalez <nestor94gonzalez@gmail.com>
2019-02-18 18:51:53 +0700

    Reduce loop speed to 1 ms in contact_publisher

--- mara_contact_publisher/src/ContactPublisher.cpp
+++ mara_contact_publisher/src/ContactPublisher.cpp
@@ -96,7 +96,7 @@ int main(int argc, char ** argv)
 
   // Busy wait loop...replace with your own code as needed.
   while (true)
-    gazebo::common::Time::MSleep(10);
+    gazebo::common::Time::MSleep(1);
 
   // Make sure to shut everything down.
   gazebo::client::shutdown();
