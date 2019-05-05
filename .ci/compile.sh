b5141d5401767b2b76e3dd2a918f712323ad652b
LanderU <lander.usategui@gmail.com>
2019-05-05 10:53:25 +0200

    Copy folders instead of move it

--- .ci/compile.sh
+++ .ci/compile.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-rm -rf src/MARA #Remove downloaded MARA to build current Branch
-mv /tmp/MARA src/MARA
-unset ROS_DISTRO
+rm -rf src/MARA/* #Remove downloaded MARA to build current Branch
+cp -r /tmp/MARA/* src/MARA
+#unset ROS_DISTRO
 source /opt/ros/$ROS2_DISTRO/setup.bash
 colcon build --merge-install --packages-skip individual_trajectories_bridge
 # Build ROS-ROS 2.0 Bridge
-#source /opt/ros/$ROS_DISTRO/setup.bash
-#colcon build --merge-install --packages-select individual_trajectories_bridge
-#sed -i 's#/opt/ros/melodic#/opt/ros/crystal#g' ~/ros2_mara_ws/install/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
+colcon build --merge-install --packages-select individual_trajectories_bridge
+sed -i 's#/opt/ros/melodic#/opt/ros/crystal#g' /root/ros2_mara_ws/install/setup.bash
 #Build ROS workspace
-#mkdir -p /root/catkin_mara_ws/src
-#cd /root/catkin_mara_ws/src
+mkdir -p /root/catkin_mara_ws/src
+cd /root/catkin_mara_ws/src
 git clone https://github.com/AcutronicRobotics/MARA_ROS1
-#cd ..
-#catkin_make_isolated --install
+cd ..
+catkin_make_isolated --install
