d1002b2608c30ce3e9694b485b642ed1da2775cd
LanderU <lander.usategui@gmail.com>
2019-05-05 11:12:25 +0200

    Use mara instead of MARA:
    * source in order to compile MARA_ROS1

--- .ci/compile.sh
+++ .ci/compile.sh
@@ -2,9 +2,8 @@
 
 set -e
 
-rm -rf src/MARA/* #Remove downloaded MARA to build current Branch
-mkdir src/MARA
-cp -r /tmp/MARA/* src/MARA
+rm -rf src/mara/* #Remove downloaded MARA to build current Branch
+cp -r /tmp/MARA/* src/mara
 #unset ROS_DISTRO
 source /opt/ros/$ROS2_DISTRO/setup.bash
 colcon build --merge-install --packages-skip individual_trajectories_bridge
@@ -14,6 +13,8 @@ colcon build --merge-install --packages-select individual_trajectories_bridge
 sed -i 's#/opt/ros/melodic#/opt/ros/crystal#g' /root/ros2_mara_ws/install/setup.bash
 echo "Build MARA_ROS1"
 #Build ROS workspace
+source /root/ros2_mara_ws/install/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 mkdir -p /root/catkin_mara_ws/src
 cd /root/catkin_mara_ws/src
 git clone https://github.com/AcutronicRobotics/MARA_ROS1
