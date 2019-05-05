c977e5c8e626f3dae31656667829fe999beff821
LanderU <lander.usategui@gmail.com>
2019-05-05 11:28:46 +0200

    Remove MARA_ROS1

--- .ci/compile.sh
+++ .ci/compile.sh
@@ -11,12 +11,3 @@ colcon build --merge-install --packages-skip individual_trajectories_bridge
 source /opt/ros/$ROS_DISTRO/setup.bash
 colcon build --merge-install --packages-select individual_trajectories_bridge
 sed -i 's#/opt/ros/melodic#/opt/ros/crystal#g' /root/ros2_mara_ws/install/setup.bash
-echo "Build MARA_ROS1"
-#Build ROS workspace
-source /root/ros2_mara_ws/install/setup.bash
-source /opt/ros/$ROS_DISTRO/setup.bash
-mkdir -p /root/catkin_mara_ws/src
-cd /root/catkin_mara_ws/src
-git clone https://github.com/AcutronicRobotics/MARA_ROS1
-cd ..
-catkin_make_isolated --install
