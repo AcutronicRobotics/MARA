b910c057203cc577d349c8fe3df7b4af145f9fbc
LanderU <lander.usategui@gmail.com>
2019-05-05 10:26:18 +0200

    Update compile.sh:
    * Try to compile only MARA ROS2 ws

--- .ci/compile.sh
+++ .ci/compile.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-#rm -rf src/MARA #Remove downloaded MARA to buildld --merge-install --packages-skip individual_trajectories_bridge 
+#rm -rf src/MARA #Remove downloaded MARA to build current branch
 source /opt/ros/$ROS2_DISTRO/setup.bash
 colcon build --merge-install --packages-skip individual_trajectories_bridge
 # Build ROS-ROS 2.0 Bridge
-source /opt/ros/$ROS_DISTRO/setup.bash
-colcon build --merge-install --packages-select individual_trajectories_bridge
-sed -i 's#/opt/ros/melodic#/opt/ros/crystal#g' ~/ros2_mara_ws/install/setup.bash
+#source /opt/ros/$ROS_DISTRO/setup.bash
+#colcon build --merge-install --packages-select individual_trajectories_bridge
+#sed -i 's#/opt/ros/melodic#/opt/ros/crystal#g' ~/ros2_mara_ws/install/setup.bash
 #Build ROS workspace
-mkdir -p /root/catkin_mara_ws/src
-cd /root/catkin_mara_ws/src
+#mkdir -p /root/catkin_mara_ws/src
+#cd /root/catkin_mara_ws/src
 git clone https://github.com/AcutronicRobotics/MARA_ROS1
-cd ..
-catkin_make_isolated --install
+#cd ..
+#catkin_make_isolated --install
