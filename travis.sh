28a9c263f7b218511dc988d708629718c33a866e
LanderU <lander.usategui@gmail.com>
2019-06-22 08:45:28 +0200

    travis.sh fix linter errors:
    * Fix SC2164
    * Fix SC2086
    * Fix S2016
    
    Signed-off-by: LanderU <lander.usategui@gmail.com>

--- travis.sh
+++ travis.sh
@@ -3,6 +3,8 @@
 Author: Lander Usategui, lander at erlerobotics dot com
 '
 
+set -e
+
 # Export colors
 export RED="\033[31m"
 export GREEN="\033[32m"
@@ -17,14 +19,14 @@ export ROS2_SOURCE="/opt/ros/${ROS2_DISTRO}/"
 export ROS1_SOURCE="/opt/ros/${ROS1_DISTRO}/"
 
 # Colcon flags
-export COLCON_COMMAND="--merge-install"
-export COLCON_SKIP_PACKAGES="--packages-skip individual_trajectories_bridge"
+export COLCON_COMMAND_FLAGS="--merge-install"
+export COLCON_SKIP_PACKAGES="individual_trajectories_bridge"
 
 
 function prepare_ws()
 {
   echo -e "${YELLOW}Download dependencies for current branch${RESET}"
-  cd ${WS}
+  cd ${WS} || exit
   rm -rf mara-ros2.repos
   cp -r /tmp/mara src && cp src/mara/mara-ros2.repos .
   sed -i '2,5d' mara-ros2.repos
@@ -41,10 +43,10 @@ function prepare_ws()
 function run_rosdep()
 {
   echo -e "${YELLOW}Making sure that everything is installed${RESET}"
-  cd ${WS}
+  cd ${WS} || exit
   apt update -qq && rosdep update
   rosdep install -q -y --from-paths . --ignore-src --rosdistro \
-                                  ${ROS2_DISTRO} --skip-keys \
+                                  "${ROS2_DISTRO}" --skip-keys \
                                   "hrim_actuator_rotaryservo_actions \
                                    hrim_actuator_gripper_srvs \
                                    hrim_actuator_rotaryservo_msgs \
@@ -66,7 +68,7 @@ function setup_hrim()
 {
   echo -e "${YELLOW}Installing hrim cli${RESET}"
   pip3 install hrim 
-  cd ${WS}/src/HRIM
+  cd ${WS}/src/HRIM || exit
   hrim generate models/actuator/servo/servo.xml
   result=$?
   hrim generate models/actuator/gripper/gripper.xml
@@ -82,8 +84,8 @@ function setup_hrim()
 function compile_ws()
 {
   echo -e "${YELLOW}Compile the WS for ROS2${RESET}"
-  cd ${WS}
-  colcon build ${COLCON_COMMAND} ${COLCON_SKIP_PACKAGES}
+  cd ${WS} || exit
+  colcon build "${COLCON_COMMAND_FLAGS}" --packages-skip "${COLCON_SKIP_PACKAGES}"
   result=$?
   if [ $result -ne 0 ]; then
     echo -e "${RED}Error compiling the ws${RESET}"
@@ -97,11 +99,11 @@ function compile_ros_bridge()
 {
   echo -e "${YELLOW}Compile bridge for ROS melodic${RESET}"
   unset ROS_DISTRO && unset ROS2_DISTRO
-  source ${ROS1_SOURCE}setup.bash
-  cd ${WS}
-  colcon build --merge-install --packages-select individual_trajectories_bridge
+  source "${ROS1_SOURCE}setup.bash"
+  cd ${WS} || exit
+  colcon build "${COLCON_COMMAND_FLAGS}" --packages-select "$COLCON_SKIP_PACKAGES"
   export ROS1_DISTRO=melodic && export ROS2_DISTRO=dashing
-  sed -i 's#/opt/ros/${ROS1_DISTRO}#/opt/ros/${ROS2_DISTRO}#g' ${WS}/install/setup.bash
+  sed -i "s#/opt/ros/${ROS1_DISTRO}#/opt/ros/${ROS2_DISTRO}#g" ${WS}/install/setup.bash
   result=$?
   if [ $result -ne 0 ]; then
     echo -e "${RED}Error compiling the bridge${RESET}"
@@ -114,9 +116,9 @@ function compile_ros_bridge()
 function ros1_ws()
 {
  mkdir -p ${ROS1_WS}/src
- cd ${ROS1_WS}/src
+ cd "${ROS1_WS}"/src || exit
  # Check if branch exists
- git ls-remote --heads https://github.com/AcutronicRobotics/MARA_ROS1 | grep ${MARA_ROS1_BRANCH} >/dev/null
+ git ls-remote --heads https://github.com/AcutronicRobotics/MARA_ROS1 | grep "${MARA_ROS1_BRANCH}" >/dev/null
  result=$?
  if [ $result -eq 1 ]; then
    echo -e "${RED}Cannot locate branch for MARA_ROS1${RESET}"
@@ -125,7 +127,7 @@ function ros1_ws()
    # Branch exits try to download It
    tries=0
    echo -e "${YELLOW}Downloading MARA_ROS1${RESET}"
-   while [ $(git clone -b ${MARA_ROS1_BRANCH} https://github.com/AcutronicRobotics/MARA_ROS1) ]; do
+   while [ "$(git clone -b "${MARA_ROS1_BRANCH}" https://github.com/AcutronicRobotics/MARA_ROS1)" ]; do
      echo -e "${YELLOW}Trying to download MARA_ROS1, try number: ${tries}${RESET}"
     /bin/sleep 2
     if [ $tries -eq 3 ]; then
@@ -135,7 +137,7 @@ function ros1_ws()
     fi
    done
  fi
- cd ${ROS1_WS}
+ cd ${ROS1_WS} || exit
  echo -e "${BLUE}Compile MARA_ROS1${RESET}"
  catkin_make_isolated --install
  result=$?
